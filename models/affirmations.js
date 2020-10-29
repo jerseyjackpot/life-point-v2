6bc922dcd7c6ce3d9d26c700cd5db47498aff190
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-10-29 16:39:17 -0400

    initial commit of react

--- models/affirmations.js
+++
@@ -1,8 +0,0 @@
-// Model for the affirmations
-module.exports = function(sequelize, DataTypes) {
-  const Affirmation = sequelize.define("Affirmation", {
-    quote: { type: DataTypes.STRING }
-  });
-
-  return Affirmation;
-};
