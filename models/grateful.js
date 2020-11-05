0ade68cd942fb236388bd491e020b31bde62d99d
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-11-04 19:50:29 -0500

    removed unnecessary brackets

--- models/grateful.js
+++ models/grateful.js
@@ -23,11 +23,10 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `grateful` array in User.js
 const gratefulSchema = new Schema({
-  one: [
-    {
-      type: String,
-    },
-  ],
+  one:
+  {
+    type: String,
+  },
   two: {
     type: String,
   },
