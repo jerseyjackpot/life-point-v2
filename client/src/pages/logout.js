0c2a70468d07b5aff0b2a27bec6c97072d93fd42
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-12-09 00:41:29 -0500

    fixed issue in api routes so that the data saves from the entry. now making a logout page

---
+++ client/src/pages/logout.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./style.css";
+// import { fadeInUp } from 'react-animations';
+// import Radium, { StyleRoot } from 'radium';
+// import "animate.css/animate.min.css";
+
+// const styles = {
+//     fadeInUp: {
+//         animation: 'x 1.5s',
+//         animationName: Radium.keyframes(fadeInUp, 'fadeInUp')
+//     }
+//   }
+  
+
+function Logout() {
+    return (
+
+     <div className="container">
+        <div className="row">
+            hi
+            {/* <StyleRoot><h1 className="text-center" style={styles.fadeInUp}>Take Care!</h1>
+            <p className="text-center"style={styles.fadeInUp} >Thank you for taking the time to use Life Point today. 
+            We hope you're feeling better. See you soon! </p></StyleRoot> */}
+
+</div>
+</div> 
+
+
+  );
+}
+
+export default Logout;
